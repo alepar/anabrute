3aa0304379478c2a131043bd58c22a1465cf006e
Alexey Parfenov <parfenov@amazon.com>
2017-04-06 15:02:01 -0700

    md5 kernel

--- main.c
+++ main.c
@@ -8,11 +8,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <unitypes.h>
+#include <time.h>
+#include <event.h>
 
-#define die_unless(val, msg) \
-if (!(val)) {\
-    fprintf(stderr, "FATAL: %s\n", msg);\
-    return -2;\
+#define die_iferr(val, msg) \
+if (val) {\
+    fprintf(stderr, "FATAL: %d - %s\n", val, msg);\
+    return val;\
 }
 
 uint32_t num_cpu_cores() {
@@ -42,14 +44,41 @@ char* read_file(const char* filename) {
 }
 
 
-static const char* size_suffixes[] = {"", "KiB", "MiB", "GiB", "TiB", "PiB"};
-void format_size(uint64_t size, char* dst) {
+static const char* size_suffixes[] = {"", "K", "M", "G", "T", "P"};
+void format_bignum(uint64_t size, char *dst, uint16_t div) {
     int divs = 0;
-    while (size/1024 > 1) {
-        size = size/1024;
+    while (size/div > 1) {
+        size = size/div;
         divs++;
     }
     sprintf(dst, "%lu%s", size, size_suffixes[divs]);
+}
+
+void hash_to_ascii(uint32_t *hash_ints, char *buf) {
+    int di = 0;
+    for(int si=0; si<4; si++) {
+        buf[di++] = (hash_ints[si] & 0x000000f0) >>  4;
+        buf[di++] = (hash_ints[si] & 0x0000000f)      ;
+
+        buf[di++] = (hash_ints[si] & 0x0000f000) >> 12;
+        buf[di++] = (hash_ints[si] & 0x00000f00) >>  8;
+
+        buf[di++] = (hash_ints[si] & 0x00f00000) >> 20;
+        buf[di++] = (hash_ints[si] & 0x000f0000) >> 16;
+
+        buf[di++] = (hash_ints[si] & 0xf0000000) >> 28;
+        buf[di++] = (hash_ints[si] & 0x0f000000) >> 24;
+    }
+
+    for(int i=0; i<32; i++) {
+        if (buf[i] > 9) {
+            buf[i] += 'a' - 10;
+        } else {
+            buf[i] += '0';
+        }
+    }
+
+    buf[di] = 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -59,13 +88,13 @@ int main(int argc, char *argv[]) {
     cl_platform_id platform_id;
     cl_uint num_platforms;
     clGetPlatformIDs (1, &platform_id, &num_platforms);
-    die_unless(num_platforms, "no platforms");
+    die_iferr(!num_platforms, "no platforms");
 
     cl_uint num_devices;
     clGetDeviceIDs (platform_id, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
     cl_device_id device_ids[num_devices];
     clGetDeviceIDs (platform_id, CL_DEVICE_TYPE_ALL, num_devices, device_ids, &num_devices);
-    die_unless(num_devices, "no devices");
+    die_iferr(!num_devices, "no devices");
 
     uint32_t num_gpus = 0;
     for (int i=0; i<num_devices; i++) {
@@ -97,69 +126,81 @@ int main(int argc, char *argv[]) {
         clGetDeviceInfo(device_ids[i], CL_DEVICE_LOCAL_MEM_SIZE, 8, &local_mem, NULL);
         clGetDeviceInfo (device_ids[i], CL_DEVICE_NAME, 1024, char_buf, NULL);
 
-        format_size(global_mem, global_mem_str);
-        format_size(local_mem, local_mem_str);
-        printf("Using OpenCL device: %s (g:%s l:%s)\n", char_buf, global_mem_str, local_mem_str);
+        format_bignum(global_mem, global_mem_str, 1024);
+        format_bignum(local_mem, local_mem_str, 1024);
+        printf("OpenCL device #%d: %s (g:%siB l:%siB)\n", i+1, char_buf, global_mem_str, local_mem_str);
     }
+    printf("\n");
 
     const cl_context_properties ctx_props [] = { CL_CONTEXT_PLATFORM, platform_id, 0, 0 };
 
     cl_int errcode;
     cl_context ctx = clCreateContext (ctx_props, num_devices, device_ids, NULL, NULL, &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to create context");
+    die_iferr(errcode, "failed to create context");
 
-    char *const kernel_source = read_file("kernels/saxpy.cl");
-    die_unless(kernel_source, "failed to read kernel source");
-    
+    // loading kernel
+    char *const kernel_source = read_file("kernels/md5.cl");
+    die_iferr(!kernel_source, "failed to read kernel source");
     size_t lengths[] = {strlen(kernel_source)};
     const char *sources[] = {kernel_source};
-
     cl_program program = clCreateProgramWithSource(ctx, 1, sources, lengths, &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to create program");
-
+    die_iferr(errcode, "failed to create program");
     errcode = clBuildProgram (program, num_devices, device_ids, NULL, NULL, NULL);
-    die_unless(errcode == CL_SUCCESS, "failed to build program");
+    die_iferr(errcode, "failed to build program");
+    cl_kernel md5_kernel = clCreateKernel (program, "md5", &errcode);
+    die_iferr(errcode, "failed to create kernel");
 
-    cl_kernel kernel = clCreateKernel (program, "SAXPY", &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to create kernel");
+    // loading data
+    uint32_t data_info[2];
+    uint32_t keys[16];
+    uint32_t hashes[4];
 
-    float a[1024], b[1024];
-    for(int i=0; i<1024; i++ ) {
-        a[i] = i;
-        b[i] = 1024-i;
-    }
+    data_info[0] = 36; // KEY_LENGTH, 9 uints per key
+    data_info[1] = 1;  // num_keys
+    char *key = "tyranous pluto twits";
+    memcpy(keys, key, strlen(key)+1);
 
-    cl_mem a_buf = clCreateBuffer (ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof (a), a, &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to copy a");
-    cl_mem b_buf = clCreateBuffer (ctx, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof (b), b, &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to copy b");
+    cl_mem data_info_buf = clCreateBuffer (ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof (data_info), data_info, &errcode);
+    die_iferr(errcode, "failed to create data_info_buf");
+    cl_mem keys_buf = clCreateBuffer (ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof (keys), keys, &errcode);
+    die_iferr(errcode, "failed to create keys_buf");
+    cl_mem hashes_buf = clCreateBuffer (ctx, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof (hashes), hashes, &errcode);
+    die_iferr(errcode, "failed to create hashes_buf");
 
+    // running kernel
     cl_command_queue queue = clCreateCommandQueue(ctx, device_ids[0], NULL, &errcode);
-    die_unless(errcode == CL_SUCCESS, "failed to create command queue");
+    die_iferr(errcode, "failed to create command queue");
 
-    clSetKernelArg(kernel, 0, sizeof (cl_mem), &a_buf);
-    clSetKernelArg(kernel, 1, sizeof (cl_mem), &b_buf);
-    const float two = 2.0f;
-    clSetKernelArg (kernel, 2, sizeof (float), &two);
+    clSetKernelArg(md5_kernel, 0, sizeof (cl_mem), &data_info_buf);
+    clSetKernelArg(md5_kernel, 1, sizeof (cl_mem), &keys_buf);
+    clSetKernelArg(md5_kernel, 2, sizeof (cl_mem), &hashes_buf);
 
-    const size_t globalWorkSize [] = { 1024, 0, 0 };
-    errcode = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, globalWorkSize, NULL, 0, NULL, NULL);
-    die_unless(errcode == CL_SUCCESS, "failed to enqueue kernel");
 
-    float b_res[1024];
-    errcode = clEnqueueReadBuffer (queue, b_buf, CL_TRUE, 0, sizeof (float) * 1024, b_res, 0, NULL, NULL);
-    die_unless(errcode == CL_SUCCESS, "failed to read buffer");
+    struct timeval t0;
+    gettimeofday(&t0, 0);
+    const size_t globalWorkSize [] = { 1024, 1024, 1024 };
+    errcode = clEnqueueNDRangeKernel(queue, md5_kernel, 3, NULL, globalWorkSize, NULL, 0, NULL, NULL);
+    die_iferr(errcode, "failed to enqueue kernel");
 
-/*    for(int i=0; i<1024; i++ ) {
-        printf("%.2f\t%.2f\t%.2f\n", a[i], b[i], b_res[i]);
-    }*/
+    errcode = clEnqueueReadBuffer (queue, hashes_buf, CL_TRUE, 0, sizeof (hashes), hashes, 0, NULL, NULL);
+    die_iferr(errcode, "failed to read hashes");
+
+    struct timeval t1;
+    gettimeofday(&t1, 0);
+    long elapsed_millis = (t1.tv_sec-t0.tv_sec)*1000 + (t1.tv_usec-t0.tv_usec)/1000;
+    format_bignum(1000L*1024*1024*1024/elapsed_millis, char_buf, 1000);
+    printf("kernel took %.2fsec, speed: ~%sHash/s\n", elapsed_millis/10/100.0, char_buf);
+
+    hash_to_ascii(hashes, char_buf);
+    printf("hash: %s\n", char_buf);
 
     clReleaseCommandQueue (queue);
 
-    clReleaseMemObject(b_buf);
-    clReleaseMemObject(a_buf);
+    clReleaseMemObject(data_info_buf);
+    clReleaseMemObject(keys_buf);
+    clReleaseMemObject(hashes_buf);
 
-    clReleaseKernel(kernel);
+    clReleaseKernel(md5_kernel);
     clReleaseProgram(program);
 
     clReleaseContext(ctx);
